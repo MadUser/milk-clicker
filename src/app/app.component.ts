f1100d911f2510bb530c4f44075fb58523c40e62
Mad User <mr_van_halen@hotmail.com>
2025-02-26 23:28:07 +0200

    Initialize Angular project with basic structure and configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  counter = 0;
-  dropMulti = 0;
-  dropPrice = 15;
-
-  farmMulti =0;
-  farmPrice = 100;
-  farmMilkPerSec = 4;
-  
-  ngOnInit() {
-    setInterval(() => this.calc(), 100);
-  }
-
-  onClick() {
-    this.counter++;
-  } 
-  
-  onFarmClick() {
-    this.farmMulti++;
-    this.counter = this.counter - this.farmPrice;
-    this.farmPrice = Math.ceil(this.farmPrice * 1.15);
-  }
-
-  calc() {
-    this.counter = this.counter + ((this.farmMulti * this.farmMilkPerSec)/10);
-  }
+export class AppComponent {
+  title = 'milk-clicker';
 }
-
