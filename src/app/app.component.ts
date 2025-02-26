1c3fabd3a72c0e428ab797b59eedbef155ef6ff9
Mad User <mr_van_halen@hotmail.com>
2025-02-27 00:17:04 +0200

    Implement game over functionality and update milk decrement logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,10 +7,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   milk = 0;
-
+  gameOver = false;
+  
   clickCow() {
     this.milk++;
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.milk -= 10;
+      if(this.milk < 0) {
+        this.gameOver = true;
+      }
+    }, 10000);
   }
 }
