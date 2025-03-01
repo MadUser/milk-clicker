a58e67314ae9145f1cdeaa303df843aae4d3fe5b
Mad User <mr_van_halen@hotmail.com>
2025-03-01 22:26:26 +0200

    add click animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+  animationCounter = 1;
+  animationItems = new Array(10).fill(false);
+  clickValue = 1;
   milk = 0;
   gameOver = false;
   
   clickCow() {
-    this.milk++;
+    this.milk+=this.clickValue;
+    this.setAnimation();
   }
 
   ngOnInit() {
@@ -23,4 +28,13 @@ export class AppComponent implements OnInit {
       }
     }, 10000);
   }
+
+  setAnimation() {
+    this.animationItems[this.animationCounter] = true;
+    this.animationCounter++;
+    if(this.animationCounter > 9) {
+      this.animationCounter = 0;
+    }
+  }
+  
 }
